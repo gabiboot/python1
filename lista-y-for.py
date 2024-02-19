58b0e23369e9e396aaae65551d98505616045f86
Gabriela <123262079+gabiboot@users.noreply.github.com>
2024-02-19 11:38:20 -0300

    Add files via upload

---
+++ lista-y-for.py
@@ -0,0 +1,7 @@
+lista = ["mauro","andrea","gael", "pepe"];
+for n in lista :
+    if n == "andrea":
+     indiceLista = lista.index(n)
+     print(f"nombre es {n} con indice {indiceLista}")
+""" else:
+     print( "nop")    """ 
