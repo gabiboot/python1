b76fe340cc2080b23e2726fd9407e973c0ce5801
Gabriela <123262079+gabiboot@users.noreply.github.com>
2024-02-20 15:44:52 -0300

    Add files via upload

---
+++ lista-de-colores.py
@@ -0,0 +1,7 @@
+"""Escribe un programa en Python que cree una lista de colores y luego muestre cada color en una línea separada."""
+
+colores =["azul", "blanco", "magenta", "carmesi", "violeta", "amarillo"]
+contador = 0
+for i in colores:
+    contador += 1
+    print(f"el color numero {contador} es {i}")
